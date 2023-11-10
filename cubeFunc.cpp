00f9e2a4c798306276507e24901069908fd31e3b
Raisa Silea <rs1753@live.mdx.ac.uk>
2023-11-10 14:58:39 +0000

    final commit

--- cubeFunc.cpp
+++ cubeFunc.cpp
@@ -3,10 +3,16 @@ using namespace std;
 
 
 double Cube(double num){
-  cube = num * num * num;
+  double cube = num * num * num;
   return cube;
 }
 
 int main(){
-  
+  double x;
+
+  cout << "Enter a number";
+  cin >> x ;
+
+  double result = Cube(x);
+  cout << "The cube of " << x << " is: " << result<< endl; 
 }
