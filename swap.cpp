d8d8f28544bf4d4e09dd27e2213c6760a972989a
Raisa Silea <rs1753@live.mdx.ac.uk>
2023-11-10 15:49:22 +0000

    final commit

--- swap.cpp
+++ swap.cpp
@@ -7,7 +7,18 @@ void swap(int array[], int pos1, int pos2){
   array[pos2] = temp;
 }
 
+void printArray(const int a[], int len) {
+  cout <<"[";
+
+  for (int i =0; i < len; ++i) {
+    cout << a[i] << ",";
+  }
+  cout << "]" << endl;
+}
+
 int main(){
   int numbers[] = {1, 2, 3};
+  printArray(numbers,3);
   swap(numbers, 0, 1);
+  printArray(numbers,3);
 }
